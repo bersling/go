a3fb5fe14d9279f6d34a0438040c736518a10b93
bersling <bersling@gmail.com>
2018-03-19 23:04:53 +0100

    add arrays note

--- tuts/arrays/arrays.go
+++ tuts/arrays/arrays.go
@@ -7,6 +7,8 @@ import "fmt"
 
 func main() {
 
+	// DN: Arrays have a preset length.
+
 	// Here we create an array `a` that will hold exactly
 	// 5 `int`s. The type of elements and length are both
 	// part of the array's type. By default an array is
