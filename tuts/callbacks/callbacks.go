5f9ccfe4ecda46f29fd5f331aa45711b04f61265
bersling <bersling@gmail.com>
2018-03-21 08:27:42 +0100

    use external cb fun

--- tuts/callbacks/callbacks.go
+++ tuts/callbacks/callbacks.go
@@ -9,9 +9,7 @@ import (
 
 func main() {
 	fmt.Println("hi")
-	go readFile(func(returnval string) {
-		fmt.Println(returnval)
-	})
+	go readFile(callbackFunction)
 	fmt.Println("goodbye (press a key to leave the program)")
 
 	fmt.Scanln()
@@ -21,4 +19,8 @@ func main() {
 func readFile(cb func(string)) {
 	time.Sleep(time.Second)
 	cb("Lorem ipsum...")
+}
+
+func callbackFunction(returnval string) {
+	fmt.Println(returnval)
 }
